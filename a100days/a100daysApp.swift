d3ed0eb2443d9544717c774deca34f4afbbcca1f
JefferssonSemin <jeffesemin@hotmail.com>
2021-07-20 16:16:15 -0300

    Initial Commit

---
+++ a100days/a100daysApp.swift
@@ -0,0 +1,17 @@
+//
+//  a100daysApp.swift
+//  a100days
+//
+//  Created by Jeffersson Semin on 20/07/21.
+//
+
+import SwiftUI
+
+@main
+struct a100daysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
