ebddb6d155d7ff14f5d35d885d0ade3362898788
JefferssonSemin <jeffesemin@hotmail.com>
2021-07-21 15:59:31 -0300

    chore: arrays, conjuntos e tuples

--- a100days/ContentView.swift
+++ a100days/ContentView.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    var age = """
+        19\
+        wer\
+        sdrew
+        """
+    var check = false
+    var pi = 3.15
+    let fixedValue = "teste"
+    
+    let types: Bool = false
+    
+    let jhon = "jhon frusciante"
+    let flea = "flea"
+    let antony = "Anthony Kiedis"
+    let chad = "chad smitt"
+    
+    var name = (descricao: "jeffersson", sobre: "semin")
+    
+    let rhcpSet = Set(["a", "b", "c", "d", "a"])
+    let rhcp = ["a", "b", "c", "d", "a"]
+    
     var body: some View {
-        Text("Hello, world!")
+        Text(name.1)
             .padding()
     }
 }
