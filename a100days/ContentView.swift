30b906c92a6f2bea47c5a7cdf6c1ace11e6d89f8
JefferssonSemin <jeffesemin@hotmail.com>
2021-07-21 16:19:46 -0300

    chore: day 2, Complex data types

--- a100days/ContentView.swift
+++ a100days/ContentView.swift
@@ -26,13 +26,30 @@ struct ContentView: View {
     
     var name = (descricao: "jeffersson", sobre: "semin")
     
+    //var dicionario = Dictionary<String, String>()
+     var dicionario = ["Calha": "É um objeto de aluminio que capta a agua"]
+    
     let rhcpSet = Set(["a", "b", "c", "d", "a"])
     let rhcp = ["a", "b", "c", "d", "a"]
     
     var body: some View {
-        Text(name.1)
+        Text(dicionario["Calha", default:"asd"])
             .padding()
     }
+}
+
+enum Teste {
+    case sucess
+    
+}
+
+enum TesteValor {
+    case sucess(valor: String)
+}
+
+enum TesteEnumerado : Int {
+    case ac // Esse será o numero 0
+    case ab = 1
 }
 
 struct ContentView_Previews: PreviewProvider {
